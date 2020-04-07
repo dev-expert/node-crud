1afaf771dcd2a66b4609a14fa44858c6a78a8bf3
ujjwal <us36335@gmail.com>
2020-04-07 13:30:00 +0530

    first message

--- db.js
+++ db.js
@@ -1,2 +1,9 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
+// mongoose.connect('mongodb://localhost:27017/storage01',
+// err => {
+//     if(!err)
+//         console.log("MongoDB sucessfull")
+//     else
+//         console.log("MongoDB error")
+// })
